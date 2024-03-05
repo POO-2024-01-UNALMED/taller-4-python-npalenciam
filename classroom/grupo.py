b9c6c25e2a70df9fa6202921637bdb53fc4ae1b7
npalenciam <npalenciam@unal.edu.co>
2024-03-05 02:16:54 -0500

    ayuds

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -25,14 +25,7 @@ class Grupo:
     def __str__(self):
         return "Grupo de estudiantes: " + self._grupo
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
